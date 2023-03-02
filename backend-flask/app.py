a375c518d0aa09eaf3bccc5dc21275ba4ff579e1
rangararazin <rangararazin@outlook.com>
2023-03-02 14:07:53 +0000

    add xray sample json and config for xray in backen

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -24,6 +24,10 @@ from opentelemetry.sdk.trace.export import BatchSpanProcessor
 
 from opentelemetry.sdk.trace.export import SimpleSpanProcessor,ConsoleSpanExporter
 
+# X-RAY ----------
+from aws_xray_sdk.core import xray_recorder
+from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
+
 
 
 # Initialize tracing and an exporter that can send data to Honeycomb
@@ -37,7 +41,15 @@ provider.add_span_processor(simple_processor)
 trace.set_tracer_provider(provider)
 tracer = trace.get_tracer(__name__)
 
+# X-RAY ----------
+xray_url = os.getenv("AWS_XRAY_URL")
+xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
+
+
 app = Flask(__name__)
+
+# X-RAY ----------
+XRayMiddleware(app, xray_recorder)
 
 # Initialize automatic instrumentation with Flask
 FlaskInstrumentor().instrument_app(app)
