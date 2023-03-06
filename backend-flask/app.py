36ad61ffa334de39725f0c00d160e219109cdfd9
rangararazin <rangararazin@outlook.com>
2023-03-06 21:46:15 +0000

    add  instrument rollbar

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -3,6 +3,10 @@ from flask import request
 from flask_cors import CORS, cross_origin
 import os
 
+import rollbar
+import rollbar.contrib.flask
+from flask import got_request_exception
+
 from services.home_activities import *
 from services.notifications_activities import *
 from services.user_activities import *
@@ -78,6 +82,29 @@ cors = CORS(
   allow_headers="content-type,if-modified-since",
   methods="OPTIONS,GET,HEAD,POST"
 )
+
+#Rollbar
+rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
+@app.before_first_request
+def init_rollbar():
+    """init rollbar module"""
+    rollbar.init(
+        # access token
+        rollbar_access_token,
+        # environment name
+        'production',
+        # server root directory, makes tracebacks prettier
+        root=os.path.dirname(os.path.realpath(__file__)),
+        # flask already sets up logging
+        allow_logging_basic_config=False)
+
+    # send exceptions from `app` to rollbar, using flask's signal system.
+    got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
+
+@app.route('/rollbar/test')
+def rollbar_test():
+    rollbar.report_message('Hello World!', 'warning')
+    return "Hello World!"    
 
 # @app.after_request
 # def after_request(response):
