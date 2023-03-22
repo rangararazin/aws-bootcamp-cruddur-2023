998b45a9efa0b29029129d8d7c2f2bf8c1b2519a
rangararazin <rangararazin@outlook.com>
2023-03-22 23:49:23 +0000

    update fe and be user handle variable/prompts to resolve Null violation error

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -1,9 +1,8 @@
 from datetime import datetime, timedelta, timezone
 from opentelemetry import trace
-
 from lib.db import db
 
-#tracer = trace.get_tracer("home.activities")
+tracer = trace.get_tracer("home.activities")
 
 class HomeActivities:
   def run(cognito_user_id=None):
--- frontend-react-js/src/pages/HomeFeedPage.js
+++ frontend-react-js/src/pages/HomeFeedPage.js
@@ -1,13 +1,12 @@
 import './HomeFeedPage.css';
 import React from "react";
 
-import { Auth } from 'aws-amplify';
-
 import DesktopNavigation  from '../components/DesktopNavigation';
 import DesktopSidebar     from '../components/DesktopSidebar';
 import ActivityFeed from '../components/ActivityFeed';
 import ActivityForm from '../components/ActivityForm';
 import ReplyForm from '../components/ReplyForm';
+import { Auth } from 'aws-amplify';
 
 // [TODO] Authenication
 import Cookies from 'js-cookie'
@@ -18,8 +17,7 @@ export default function HomeFeedPage() {
   const [poppedReply, setPoppedReply] = React.useState(false);
   const [replyActivity, setReplyActivity] = React.useState({});
   const [user, setUser] = React.useState(null);
-  const dataFetchedRef = React.useRef(false);
-
+  const dataFetchedRef = React.useRef(false);  
 
   const loadData = async () => {
     try {
@@ -41,31 +39,30 @@ export default function HomeFeedPage() {
     }
   };
 
-  // check if we are authenicated
-const checkAuth = async () => {
-  Auth.currentAuthenticatedUser({
-    // Optional, By default is false. 
-    // If set to true, this call will send a 
-    // request to Cognito to get the latest user data
-    bypassCache: false 
-  })
-  .then((user) => {
-    console.log('user',user);
-    return Auth.currentAuthenticatedUser()
-  }).then((cognito_user) => {
-      setUser({
-        display_name: cognito_user.attributes.name,
-        handle: cognito_user.attributes.preferred_username
-      })
-  })
-  .catch((err) => console.log(err));
-};
-
+  const checkAuth = async () => {
+    Auth.currentAuthenticatedUser({
+      // Optional, By default is false. 
+      // If set to true, this call will send a 
+      // request to Cognito to get the latest user data
+      bypassCache: false 
+    })
+    .then((user) => {
+      console.log('user',user);
+      return Auth.currentAuthenticatedUser()
+    }).then((cognito_user) => {
+        setUser({
+          display_name: cognito_user.attributes.name,
+          handle: cognito_user.attributes.preferred_username
+        })
+    })
+    .catch((err) => console.log(err));
+  };
+  
+  // check when the page loads if we are authenicated
   React.useEffect(()=>{
-    //prevents double call
+    //prevents double call    
     if (dataFetchedRef.current) return;
     dataFetchedRef.current = true;
-
     loadData();
     checkAuth();
   }, [])
@@ -75,9 +72,10 @@ const checkAuth = async () => {
       <DesktopNavigation user={user} active={'home'} setPopped={setPopped} />
       <div className='content'>
         <ActivityForm  
+          user_handle={user}
           popped={popped}
-          setPopped={setPopped} 
-          setActivities={setActivities} 
+          setPopped={setPopped}
+          setActivities={setActivities}
         />
         <ReplyForm 
           activity={replyActivity} 
